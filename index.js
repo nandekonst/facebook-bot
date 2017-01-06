a7100cf4fa59c4a6cd40e878a8740d0a60949835
Nande Konst <nandekonst@MacBook-Air-van-Lennart-Konst.local>
2017-01-06 16:19:17 +0100

    - check if user exists - create record - store userid in Dexia - greet user and ask question 1

--- index.js
+++ index.js
@@ -2,34 +2,54 @@
 const http = require('http');
 const request = require('request');
 const express = require('express');
+const rp = require('request-promise');
+var Promise = require('promise'); 
 const bodyParser = require('body-parser');
 const app = express();
-const PAGE_ACCESS_TOKEN = 'EAATSm02f8EIBAGswWyy7ZCQVy3BvtVMedw6pYZAQMy5kIdrpvyDNWvw2ZAKcbW9OlyQPNFSu6YoGKHPdMJ4IwZAMr1dZCHU5CczZCEGQUWd9ZAKZC4HDnEA3HpODEZBQ6kBwtIZAF7ANFcBFjtFROZChQpH5C4XoM4UyYZBIDiF9j96OTAZDZD';
+const PAGE_ACCESS_TOKEN = 'EAATSm02f8EIBAE9FEhKFLjf7t8GTUxv3F2sch4kdiyt7fBiH5xV63TvlXsVfPumlKwbO8pQNlO7pVm25bVVOPn8ZAg4wp92YdMJZAO2i1C3e9S98rFC3OnMSChixmQzZAQ1xX2OKwmoYJN7RyrbLn39LtwAZAZAMmsaFP9DM7xgZDZD';
 const HELP_BUTTON = "DEVELOPER_DEFINED_PAYLOAD_FOR_HELP";
 const FAQ_BUTTON = "DEVELOPER_DEFINED_PAYLOAD_FOR_FAQ";
 const DATA_BUTTON = "DEVELOPER_DEFINED_PAYLOAD_FOR_DATA";
 var token;
-var text;
-var prop_city;
-var prop_type;
-var prop_rooms;
-var prop_price;
-var jexia_user_id;
+
+var EmptyFieldEnum = {
+
+  noEmptyField : 0,
+  locationField : 1, 
+  propertyTypeField : 2,
+  roomNumberField : 3,
+  maxPriceField : 4,
+
+}
+
 
 app.set('port', (process.env.PORT || 8080))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use (bodyParser.json())
-app.get('/', function(req, res){
-  res.send(req.query['hub.challenge'])
 
+
+app.get('/webhook', function(req, res){
+
+  if(req.query['hub.verify_token'] === 'nande'){
+    res.send(req.query['hub.challenge'])
+
+  }
+  res.send('Error, wrong token')
 })
+app.listen(app.get('port'), function(){
+
+  console.log('running on port', app.get('port'))
+})
+
+
+
 
 //Make data exchange between App and Facebook over the webhook possible
 app.post('/webhook', function (req, res) {
   var data = req.body;
 
 
-  // Make sure this is a page subscription
+ // Make sure this is a page subscription
   if (data.object === 'page') {
     // Iterate over each entry
     // There may be multiple if batched
@@ -59,40 +79,205 @@ app.post('/webhook', function (req, res) {
     //
     // You must send back a 200, within 20 seconds, to let us know you've 
     // successfully received the callback. Otherwise, the request will time out.
-    res.sendStatus(200);
+    res.sendStatus(200).send;
   }
 });
 
 
-app.get('/webhook', function(req, res){
 
-  if(req.query['hub.verify_token'] === 'nande'){
-    res.send(req.query['hub.challenge'])
+//Message was delivered to the user
+function receivedDeliveryConfirmation(messagingEvent) {
+   senderID = messagingEvent.sender.id;
+   console.log('Received delivery confirmation from id: ', senderID)
+}
 
-  }
-  res.send('Error, wrong token')
-})
-app.listen(app.get('port'), function(){
+//Message was read by the user
+function receivedMessageReadConfirmation(messagingEvent) {
+   senderID = messagingEvent.sender.id;
+   console.log('Received message read confirmation from id: ', senderID)
+}
 
-  console.log('running on port', app.get('port'))
-})
+//A message was received .
+function receivedMessage(messagingEvent) {
+  var senderID = messagingEvent.sender.id;
+  var recipientID = messagingEvent.recipient.id;
+  let timeOfMessage = messagingEvent.timestamp;
+  let message = messagingEvent.message;
+
+  console.log('Received a message for user %d and page %d at %d with message:', senderID, recipientID, timeOfMessage);
+  console.log(JSON.stringify(message));
+
+  var userRecord = getJexiaUserRecord(senderID);
+  
+  var userRec = userRecord.then(function(data){
+  var userRecorddata = data;
+  console.log("userRecorddata"+ userRecorddata)
+
+  if(userRecorddata.length === 0) {
+      console.log("userRecorddata is null" )
+
+    createJexiaUserRecord(senderID);
+    //sendGreetingMessage(senderID);
+
+    
+    }else{
+      console.log("user record is not null" + userRecorddata)
+
+    }
+
+
+    
+  })
+
 
 
   
+  var messageText = message.text;
+ //var emptyField = getFirstEmptyField(userRecord);
+  
+
+
+  /*switch (emptyField){
+    case locationField:
+      if(!checkLocationValue(messageText))
+        sendLocationMessage(senderID);
+      else {
+        storeLocation(messageText);
+        sendPropertyTypeMessage(senderID);
+      }
+    break;
+    case propertyTypeField:
+      sendPropertyTypeMessage(senderID);
+    break;
+    case roomNumberField:
+      sendRoomNumberMessage(senderID);
+    break;
+    case maxPriceField:
+      sendMaxPriceMessage(senderID);
+    break;
+    case noEmptyField:
+    break;
+  }*/
+
+  //var messageId = message.mid;
+  //var messageAttachments = message.attachments;
+
+
+    
+
+    /*if(messageText){
+      //if we receive a text message, check to see if it matches a keyword and send back the example,
+      //otherwise just echt the text we received.
+      switch (messageText){
+        case 'generic':
+          sendGenericMessage(senderID);
+          break;
+        default:
+          sendTextMessage(senderID, messageText);
+      }
+
+    } else if (messageAttachments){
+          sendTextMessage(senderID, "Message with attachment received");
+    }*/
+    
+
+}
+
+
+
+
+
+
+function sendGenericMessage(recipientId, messageText){
+  sendTextMessage(senderID, "This is a generic message")
+  console.log("this is a generic message")
+}
+
+function sendTextMessage(recipientId, messageText){
+
+  var messageData = {
+
+    'recipient': {
+      'id':recipientId
+
+    },
+    'message': {
+      'text': messageText
+    }
+  };
+
+  callSendAPI(messageData);
+
+}
+
+
+
+function sendGreetingMessage(senderID){
+      var senderID = senderID;
+
+      sendTextMessage(senderID, "Hi I am your Real Estate assistent, I am here to help you find a suitable property. ");
+
+}
+
+function sendLocationMessage(senderID){
+
+    sendTextMessage(senderID, "Please give me the postcode of the city you are looking")
+
+}
+
+function sendPropertyTypeMessage(senderID){
+
+   sendTextMessage(senderID, "Are you looking for an appartment, house or studio?")
+}
+
+function sendRoomNumberMessage(senderID){
+
+  sendTextMessage(senderID, "How many rooms should your property have?")
+}
+
+function sendMaxPriceMessage(senderID){
+
+  sendTextMessage(senderID, "What is your maximum price?")
+}
+
+
+//callsendAPI calls the Send API
+function callSendAPI(messageData) {
+  request({
+    'uri': 'https://graph.facebook.com/v2.6/me/messages',
+    'qs': {access_token: PAGE_ACCESS_TOKEN},
+    'method': 'POST',
+    'json': messageData
+
+  }, function(error, response, body){
+
+    if(!error && response.statusCode == 200){
+
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+      console.log("successfully sent message with id &s to recipient %s", messageId, recipientId);
+    }else{
+      console.error("Unable to send message");
+      console.error(response);
+      console.error(error);
+    }
+  });
+}
+
 
 //Authentication Jexia
 function getAuth(callback){
 
 var headers = {
-  key:'79514f8aa35fb6d41723f08ef044d699', 
-  secret:'308d06f94e966e5dadee5afdcfe94b7c1e2bfbe260d193a7'
+  'key':'79514f8aa35fb6d41723f08ef044d699', 
+  'secret':'308d06f94e966e5dadee5afdcfe94b7c1e2bfbe260d193a7'
 
 }
 
 var options = {
-url:' https://afe21f70-58ac-11e6-9400-bf08cc0779e0.app.jexia.com ',
-method: 'POST',
-form: headers 
+'url':' https://afe21f70-58ac-11e6-9400-bf08cc0779e0.app.jexia.com ',
+'method': 'POST',
+'form': headers 
 }
 
 
@@ -116,305 +301,69 @@ getAuth();
 
 
 
-//Creating and posting menu items 
-var headers = {
-    'User-Agent':       'Super Agent/0.0.1',
-    'Content-Type':     'application/json" -d'
-}
+function getJexiaUserRecord(userid){
 
-var menu_object = {
-  "setting_type" : "call_to_actions",
-  "thread_state" : "existing_thread",
-  "call_to_actions":[
-    {
-      "type":"postback",
-      "title":"Help",
-      "payload":HELP_BUTTON
-    },
-  {
-    "type":"postback",
-    "title":"FAQ",
-    "payload":FAQ_BUTTON
-  },
-  {
-    "type":"postback",
-    "title":"Data",
-    "payload":DATA_BUTTON
-  }
-  ]
-}
-var options = {
-  url: 'https://graph.facebook.com/v2.6/me/thread_settings?access_token=' + PAGE_ACCESS_TOKEN,
-  method: 'POST',
-  headers: headers,
-  form: menu_object
-}
-request(options, function (error, response, body){
-  if(!error && response.statusCode == 200){
-    //print response body
-    console.log(body);
-  }
+  return new Promise(function (resolve, reject){
 
-})
+    request({
+      url:'https://afe21f70-58ac-11e6-9400-bf08cc0779e0.app.jexia.com/user?user_id=' + userid,
+      method:'GET',
+      json:true,
+      headers: {'Authorization': 'Bearer ' + token}
 
+    }, function(error, response, body){
+        if(error){
 
+          console.log(error)
+        }if(body.length === 0){
+          console.log("the body is null")
+          resolve(body);
 
-//Authentication was received
-function receivedAuthentication(messagingEvent) {
-  var senderID = messagingEvent.sender.id;
-   console.log('Received authentication from id: ', senderID)
-}
-
-
-//A message was received and echoed.
-function receivedMessage(messagingEvent) {
-  var senderID = messagingEvent.sender.id;
-  var recipientId = messagingEvent.recipient.id;
-
-   console.log('Received a message from id: ', senderID + recipientId)
-    let message = messagingEvent.message;
-    text = messagingEvent.message.text.toLowerCase();
-    console.log("Dit is de tekst" + text)
-    var senderID = messagingEvent.sender.id;
-    
-     function handleUserInput(){
-
-
-        if (text && message) {
-        var greets = ['hi', 'hello', 'hi there', 'good morning', 'good afternoon', 'good evening'] 
-        var city = ['nijmegen', 'amsterdam', 'arnhem']
-        var type = ['appartment', 'house', 'studio']
-        var rooms = ['1' , '2' , '3', '4' , '5', '6', '7']
-        var price = ['250000', '350000', '450000']
-           if(greets.indexOf(text) > -1) {
-           sendMessage(senderID, "Hi I am your Real Estate assistent, please let me know in which city you are looking for a place");
-           }else if (city.indexOf(text) > -1){
-            prop_city = text;
-
-            sendMessage(senderID, "So you are looking for a place in " + text + ".Tell me if you are looking for appartment, studio or house")  
-           }else if (type.indexOf(text) > -1){
-            prop_type = text;
-            //console.log("property_type" + prop_type)
-            sendMessage(senderID, "You are looking for " + text + "Tell me the number of rooms your ideal place should have")
-           }else if(rooms.indexOf(text) > -1){
-            prop_rooms = text;
-
-            sendMessage(senderID, "The number of rooms is "  + text + " What would be your maximum price?")
-
-           }else if(price.indexOf(text) > -1){
-            prop_price = text;
-
-            sendMessage(senderID, "Ok, I have got that, let me see what I can find for you...")
-             var data = {user_id:senderID, city:prop_city, type:prop_type, rooms:prop_rooms, price:prop_price};
-             var headers = {
-            'Authorization':'Bearer ' + token
-              }
-              var options = {
-              url: 'https://afe21f70-58ac-11e6-9400-bf08cc0779e0.app.jexia.com/User',
-              headers: headers,
-              form: data
-              };
-
-
-             // Store the user_id and the message in Jexia.
-            request.post(options, callbackUserId)
-
-           }else{
-            sendMessage(senderID, "Sorry I am not that smart yet, no sentences please, only give me the name of city, type of house and number of rooms")
-
-           }
+        }else{
+           var userRecordId = body[0].user_id
+           console.log("this is the body" + userRecordId)
+           resolve(userRecordId);
 
         }
-        
-
-
-     } 
-
+   })
+  })
+}
 
 
 
-     handleUserInput();
-
-
-   
-
-
-   //Get user Data from Jexia
-  function getUserData(userid){
-
-  var headers = {
-    'Authorization':'Bearer ' + token
+//create a record and store userid in Jexia.
+function createJexiaUserRecord(userid){
+    var useridasstring = userid.toString()
+    var data = {'user_id': useridasstring}  
+    var headers = {
+    'Authorization': 'Bearer ' + token
     }
-  var options = {
 
-    url: 'https://afe21f70-58ac-11e6-9400-bf08cc0779e0.app.jexia.com/user?user_id=' + userid,
-    method: 'GET',
-    headers: headers
+    var options = {
+    'url': 'https://afe21f70-58ac-11e6-9400-bf08cc0779e0.app.jexia.com/User',
+    'headers': headers,
+    'form': data
+    };
+
+
     
-  }
-  request.get(options, userDataCallBack)
-  }
+    request.post(options, getJexiaRecordCallback)
 
-  function userDataCallBack(error, response, body){
+    function getJexiaRecordCallback(error, response, body){
 
-
-   jexia_user_id = response ;
-
-   console.log("USERID" + JSON.stringify(jexia_user_id));
-
-    console.log("SENDERID" + senderID);
-
-  }
-
-
-
-
-getUserData('1197247203681649');
-
-
-}
-
-
-
-
-function callbackUserId(error, response, body){
-    if(!error && response.statusCode == 200){
-          var body = body
-
-      }else{
-
-          console.log("this is the body" + body)
-
-      }
-
-  } 
-
-
-//Message was delivered to the user
-function receivedDeliveryConfirmation(messagingEvent) {
-  var senderID = messagingEvent.sender.id;
-   console.log('Received delivery confirmation from id: ', senderID)
-}
-
-//Message was read by the user
-function receivedMessageReadConfirmation(messagingEvent) {
-  var senderID = messagingEvent.sender.id;
-   console.log('Received message read confirmation from id: ', senderID)
-}
-
-//Postback for the menu 
-function receivedPostbackMenu(messagingEvent) {
-  var senderID = messagingEvent.sender.id;
-    console.log('Received postback from id: ', senderID)
-
-    var payload = messagingEvent.postback.payload
-    console.log("This is the payload" + payload)
-
-    if(payload == HELP_BUTTON) {
-      console.log('Help button pushed for user ' + senderID);
-      var text = "This messenger bot helps you find properties for sale based on your needs. Please tell me the city where you are looking for a property";
-      sendMessage(senderID, text);
-
-    } else if(payload == FAQ_BUTTON) {
-      console.log('FAQ button pushed for user ' + senderID);
-      sendGenericMessage(senderID);
-      
-    } else if(payload == DATA_BUTTON) {
-      console.log('Data button pushed for user ' + senderID);
-      getUserData(senderID);
+      var jexia_user_id = JSON.parse(body)
+      console.log(jexia_user_id);
     }
-}
+    
 
-//send a message to the user
-function sendMessage(recipientId, messageText) {
-    var messageData = {
-    recipient: {
-      id: recipientId
-    },
-    message: {
-      text: messageText
-    }
-  };
-
-  callSendAPI(messageData);
-}
-
-
-//send a generic template with picture and some buttons
-function sendGenericMessage(recipientId) {
-  var messageData = {
-    recipient: {
-      id: recipientId
-    },
-    message: {
-      attachment: {
-        type: "template",
-        payload: {
-          template_type: "generic",
-          elements: [{
-            title: "rift",
-            subtitle: "Next-generation virtual reality",
-            item_url: "https://www.oculus.com/en-us/rift/",               
-            image_url: "http://messengerdemo.parseapp.com/img/rift.png",
-            buttons: [{
-              type: "web_url",
-              url: "https://www.oculus.com/en-us/rift/",
-              title: "Open Web URL"
-            }, {
-              type: "postback",
-              title: "Call Postback",
-              payload: "Payload for first bubble",
-            }],
-          }, {
-            title: "touch",
-            subtitle: "Your Hands, Now in VR",
-            item_url: "https://www.oculus.com/en-us/touch/",               
-            image_url: "http://messengerdemo.parseapp.com/img/touch.png",
-            buttons: [{
-              type: "web_url",
-              url: "https://www.oculus.com/en-us/touch/",
-              title: "Open Web URL"
-            }, {
-              type: "postback",
-              title: "Call Postback",
-              payload: "Payload for second bubble",
-            }]
-          }]
-        }
-      }
-    }
-  };  
-
-  callSendAPI(messageData);
 }
 
 
 
 
-function callSendAPI(messageData) {
-  request({
-    uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token: PAGE_ACCESS_TOKEN },
-    method: 'POST',
-    json: messageData
 
-  }, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var recipientId = body.recipient_id;
-      var messageId = body.message_id;
 
-      if (messageId) {
-        console.log("Successfully sent message with id %s to recipient %s", 
-          messageId, recipientId);
-      } else {
-      console.log("Successfully called Send API for recipient %s", 
-        recipientId);
-      }
-    } else {
-      console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error);
-    }
-  });  
-}
+
 
 
 
